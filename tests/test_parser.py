8a81dc4cd9794e11b148bab773d575c5647fe4d8
BigForLy <oringan@mail.ru>
2022-08-13 00:22:58 +0500

    feat: creating test for parser function

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -16,8 +16,11 @@ class TestParser:
     def test_avg_global(self, parser: ParserManager):
         assert "".join(parser.replace("avg(((@a))) + @a", "@a", "3", True)) == "avg(((3))) + 3"
 
-    def test_if(self, parser):
-        pass
+    def test_if(self, parser: ParserManager):
+        assert "".join(parser.replace("if(@a=3,'ДА', 'Нет')", "@a", "3", False)) == "if(3=3,'ДА', 'Нет')"
 
-    def test_if_and_avg(self, parser):
-        pass
+    def test_if_and_avg_false(self, parser):
+        assert "".join(parser.replace("if(avg(@a)=3,'ДА', 'Нет')", "@a", "[3]", False)) == "if(avg(@a)=3,'ДА', 'Нет')"
+
+    def test_if_and_avg_true(self, parser):
+        assert "".join(parser.replace("if(avg(@a)=3,'ДА', 'Нет')", "@a", "[3]", True)) == "if(avg([3])=3,'ДА', 'Нет')"
