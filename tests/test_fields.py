3659061e956eff6ab54131a64e6534214d8509c5
BigForLy <oringan@mail.ru>
2022-08-15 23:42:32 +0500

    feat: creating new tests

--- tests/test_fields.py
+++ tests/test_fields.py
@@ -1,12 +1,29 @@
 import pytest
-
-from fields import BoolField
+from decimal import Decimal
+from fields import BoolField, NumericField
 
 
 @pytest.fixture
 def get_bool_field():
-    return BoolField(definition_number=0, symbol="@a", formula="", value=1, primary_key=1)
+    return BoolField(
+        definition_number=0, symbol="@a", formula="", value=1, primary_key=1
+    )
 
 
-def test_correct_bool_field(get_bool_field):
+@pytest.fixture
+def get_numeric_field():
+    return NumericField(
+        symbol="@a", formula="", value="1.201", round_to=-1, primary_key=1
+    )
+
+
+def test_correct_bool_field(get_bool_field: BoolField):
     assert get_bool_field.symbol == "@a"
+
+
+def test_correct_numeric_value(get_numeric_field: NumericField):
+    assert get_numeric_field.value == Decimal("1.201"), get_numeric_field.value
+
+
+def test_correct_numeric_round(get_numeric_field: NumericField):
+    assert get_numeric_field.value == Decimal("1.201"), get_numeric_field.value
---
+++ tests/test_gosts.py
@@ -0,0 +1,76 @@
+from fields import NumericField, StringField
+from manage import FormulaCalculation
+
+
+class TestGostR58952_3_2020Test:
+
+    def test_GostR58952_3_2020_v1(self):
+        data = [
+            NumericField(symbol="@mvp", round_to=-4, value=1, formula="", definition_number="1",
+                         primary_key=1),
+            NumericField(symbol="@mp", round_to=-4, value=2, formula="", definition_number="1",
+                         formula_check="if(@mp<@mvp,'', 'Масса пластины должна быть больше массы пластины)'",
+                         primary_key=2),
+            NumericField(symbol="@mep", round_to=-2, value=3, formula="", definition_number="1",
+                         formula_check="if(@mp<@mep,'', 'Масса пластины должна быть больше массы пластины)'",
+                         primary_key=3),
+            NumericField(symbol="@exp", formula='((@mvp-@mp)/(@mep-@mp))*100', round_to=-2, value="",
+                         definition_number="1",
+                         primary_key=4),
+            StringField(symbol="@r", formula='avg(@exp)', round_to=-2, value="Отсвутствуют", primary_key=5),
+            StringField(symbol="@abs_rash", formula='max(@exp)-min(@exp)', round_to=-2, value="Отсвутствуют",
+                        primary_key=6)
+        ]
+        result = FormulaCalculation(data).calc()
+        assert result == {1: '1', 2: '2', 3: '3', 4: '-100', 5: '-100', 6: '0'}, f"Неверное решение: {result}"
+    
+    def test_GostR58952_3_2020_v2(self):
+        data = [
+            NumericField(symbol="@mvp", round_to=-4, value=3, formula="", definition_number="1",
+                         primary_key="1"),
+            NumericField(symbol="@mp", round_to=-4, value=2, formula="", definition_number="1",
+                         formula_check="if(@mp<@mvp,\'\', \'Масса пластины должна быть больше массы пластины)\'",
+                         primary_key="2"),
+            NumericField(symbol="@mep", round_to=-2, value=3, formula="", definition_number="1",
+                         formula_check="if(@mp<@mep,\'\', \'Масса пластины должна быть больше массы пластины)\'",
+                         primary_key="3"),
+            NumericField(symbol="@exp", formula='((@mvp-@mp)/(@mep-@mp))*100', round_to=-2, value="",
+                         definition_number="1",
+                         primary_key="4"),
+            StringField(symbol="@r", formula='avg(@exp)', round_to=-2, value="Отсвутствуют", primary_key="5"),
+            StringField(symbol="@abs_rash", formula='max(@exp)-min(@exp)', round_to=-2, value="Отсвутствуют",
+                        primary_key="6")
+        ]
+        result = FormulaCalculation(data).calc()
+        assert result == {'1': '3', '2': '2', '3': '3', '4': '100', '5': '100', '6': '0'}, f"Неверное решение: {result}"
+
+    def test_GostR58952_3_2020_v3(self):
+        data = [
+            NumericField(symbol="@mvp", round_to=-4, value=3, formula = "", definition_number="1",
+                         primary_key="1"),
+            NumericField(symbol="@mp", round_to=-4, value=2, formula = "", definition_number="1",
+                         formula_check="if(@mp<@mvp,\'\', \'Масса пластины должна быть больше массы пластины)\'",
+                         primary_key="2"),
+            NumericField(symbol="@mep", round_to=-2, value=3, formula = "", definition_number="1",
+                         formula_check="if(@mp<@mep,\'\', \'Масса пластины должна быть больше массы пластины)\'",
+                         primary_key="3"),
+            NumericField(symbol="@exp", formula='((@mvp-@mp)/(@mep-@mp))*100', round_to=-2, value="",
+                         definition_number="1", primary_key="4"),
+            NumericField(symbol="@mvp", round_to=-4, value=4, formula = "", definition_number="2",
+                         primary_key="5"),
+            NumericField(symbol="@mp", round_to=-4, value=2, definition_number="2", formula = "",
+                         formula_check="if(@mp<@mvp,\'\', \'Масса пластины должна быть больше массы пластины)\'",
+                         primary_key="6"),
+            NumericField(symbol="@mep", round_to=-2, value=3, definition_number="2", formula = "",
+                         formula_check="if(@mp<@mep,\'\', \'Масса пластины должна быть больше массы пластины)\'",
+                         primary_key="7"),
+            NumericField(symbol="@exp", formula='((@mvp-@mp)/(@mep-@mp))*100', round_to=-2, value="",
+                         definition_number="2",
+                         primary_key="8"),
+
+            StringField(symbol="@r", formula='avg(@exp)', round_to=-2, value="Отсвутствуют", primary_key="9"),
+            StringField(symbol="@abs_rash", formula='max(@exp)-min(@exp)', round_to=-2, value="Отсвутствуют",
+                        primary_key="10")
+        ]
+        result = FormulaCalculation(data).calc()
+        assert result == {'1': "3", '2': "2", '3': "3", '4': "100", '5': "4", '6': "2", '7': "3", '8': "200", '9': '150', '10': '100'}, f"Неверное решение: {result}"
