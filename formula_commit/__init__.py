cc9b8699ef0421ca227abde11b40f03d6835d29d
BigForLy <oringan@mail.ru>
2022-09-29 15:10:18 +0500

    patch: updating init file

--- formula_commit/__init__.py
+++ formula_commit/__init__.py
@@ -1,7 +1,8 @@
 from .manage import FormulaCalculation
+from .fields import BoolField, NumericField, StringField
 
 
 __version__ = "1.0.0"
 
 
-__all__ = ["FormulaCalculation"]
+__all__ = ["FormulaCalculation", "BoolField", "NumericField", "StringField"]
