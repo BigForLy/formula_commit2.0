2aeb6f749751f29b0d57ed63a76d90169f36c21e
BigForLy <oringan@mail.ru>
2022-10-05 10:44:33 +0500

    feat: new version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md') as f:
 setup(
     name='formula_commit',
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
-    version='1.0.12',
+    version='1.0.13',
     license='MIT',
     license_files='LICENSE',
     author='RB387',
